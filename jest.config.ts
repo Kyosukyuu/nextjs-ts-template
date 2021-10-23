37e4240fb0e4f082dcc53717542f7efe69ca2aca
Kyosukyuu <82899136+Kyosukyuu@users.noreply.github.com>
2021-10-23 11:54:59 -0400

    converted jest config file from js to ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   collectCoverageFrom: [
     "**/*.{js,jsx,ts,tsx}",
     "!**/*.d.ts",
@@ -14,21 +14,14 @@ module.exports = {
 
     /* Handle image imports
     https://jestjs.io/docs/webpack#handling-static-assets */
-    "^.+\\.(jpg|jpeg|png|gif|webp|svg)$":
-      "<rootDir>/__mocks__/fileMock.js",
+    "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
   },
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/.next/",
-  ],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
   testEnvironment: "jsdom",
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    "^.+\\.(js|jsx|ts|tsx)$": [
-      "babel-jest",
-      { presets: ["next/babel"] },
-    ],
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
   transformIgnorePatterns: [
     "/node_modules/",
